4262782e758eca5d1d3e6c38969036f5193a8732
Jade Huynh <35jadeh@gmail.com>
2019-07-12 03:52:46 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,11 @@ function frameIt(){
 }
 
 function pressIt(){
-  
+  $(input).on('keydown', function(key) {
+  if(key.which == 83){
+      alert('G was pressed');
+  }
+});
 }
 
 function submitIt(){}
